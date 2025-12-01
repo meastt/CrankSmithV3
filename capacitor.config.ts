89ccc74e6f1fef1a8fc17e3de87d8e289d0409ae
Meastt <youremail@example.com>
2025-11-30 17:02:19 -0700

    updated build ID for app store connect submission

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.cranksmith.app',
+  appId: 'com.cranksmith.mobile',
   appName: 'CrankSmith',
   webDir: 'public',
   server: {
